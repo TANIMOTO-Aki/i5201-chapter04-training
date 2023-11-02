7c9642ccf15d5642909e26486fce657701dd50c7
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-11-02 09:47:24 +0900

    wa=5+6+7+8+9をwhile文で求めるプログラムEX4_LoopWhileSum.javaを作成しました

---
+++ src/main/java/EX4_Loop/EX4_LoopWhileSum.java
@@ -0,0 +1,11 @@
+public class EX4_LoopWhileSum {
+    public static void main(String[] args) {
+        int wa = 0, i = 5;
+
+        while (i <= 9) {
+            wa += i;
+            i += 1;
+        }
+        System.out.print(wa);
+    }
+}
