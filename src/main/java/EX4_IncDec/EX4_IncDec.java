f6e57bfe58ea1b100399446f9c03fad867fcf52f
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-11-02 09:56:39 +0900

    第４章練習問題３ EX4_IncDec.javaを作成しました

---
+++ src/main/java/EX4_IncDec/EX4_IncDec.java
@@ -0,0 +1,10 @@
+public class EX4_IncDec {
+    public static void main(String[] args) {
+        int i = 1, j = 1, k = 1, l = 1;
+
+        System.out.println("++iの結果" + ++i);
+        System.out.println("j++の結果" + j++);
+        System.out.println("--kの結果" + --k);
+        System.out.println("l--の結果" + l--);
+    }
+}
