33a00c7196ccbcd09fe47c9004588a2dddefbd32
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-11-02 09:43:51 +0900

    第４章練習問題２　wa=5+6+7+8+9をfor文で求めるプログラムEX4_LoopForSum.javaを作成しました

---
+++ src/main/java/EX4_Loop/EX4_LoopForSum.java
@@ -0,0 +1,10 @@
+public class EX4_LoopForSum {
+    public static void main(String[] args) {
+        int wa = 0;
+
+        for (int i = 5; i <= 9; i++){
+            wa += i;
+        }
+        System.out.print(wa);
+    }
+}
